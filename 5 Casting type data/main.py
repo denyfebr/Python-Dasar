903e62c3ab5721c59d897a25311e22afc7c48813
denyfebr <denyfebri@gamil.com>
2024-12-31 13:57:21 +0700

    type data casting

---
+++ 5 Casting type data/main.py
@@ -0,0 +1,15 @@
+# Belajar casting
+# merubah dari satu tipe ke tipe lain
+# tipe data = int, float, str, bool
+
+data_int = 9
+print("data = ", data_int,",type =",type(data_int))
+
+data_float = float(data_int)
+print("data = ",data_float,",type =",type(data_float))
+
+data_str = str(data_int)
+print("data = ",data_str,",type =",type(data_str))
+
+data_bool = bool(data_int)
+print("data = ",data_bool,",type =",type(data_bool))
