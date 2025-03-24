c887d722e0b3edbb09e06f8e780b98bdd8e37187
Deny Febriyanto <denyfebri@gmail.com>
2025-03-25 02:49:27 +0700

    Latihan bitwise dan komparasi

--- 12 Tugas Komparasi dan Logika/main.py
+++ 12 Tugas Komparasi dan Logika/main.py
@@ -4,6 +4,10 @@
 
 # ----0++++5----8++++11----
 
-inputUser = float(input("Masukkan angka yang bernilai:\n1.lebih dari 0\natau\n2.kurang dari 5\natau\n3.lebih dari 8\natau\n4.kurang dari 11\n:"))
-isLebihNol = inputUser>0;
-print(isLebihNol)
+inputUser = float(input("Masukkan angka yang bernilai:\n1.lebih dari 0\ndan\n2.kurang dari 5\ndan\n3.lebih dari 8\ndan\n4.kurang dari 11\n:"))
+isLebihNol = inputUser>0
+isKurangLima = inputUser<5
+isLebihDelapan = inputUser>8
+isKurangSebelas = inputUser<11
+isCorrect = (isLebihNol and isKurangLima) or (isLebihDelapan and isKurangSebelas)
+print(isCorrect)
--- 12a Tugas Komparasi dan Logika/main.py
+++ 12a Tugas Komparasi dan Logika/main.py
@@ -4,4 +4,11 @@
 
 # ++++0----5++++8----11++++
 
-inputUser = float(input("Masukkan angka yang bernilai\nkurang dari 3\natau\nlebih besar dari 10\n:"))
+inputUser = float(input("Masukkan angka yang bernilai\nkurang dari 0\natau\nlebih besar dari 5 dan kurang dari 8\n:"))
+
+isKurangNol = inputUser<0
+isLebihLima = inputUser>5
+isKurangDelapan = inputUser<8
+isLebihSebelas = inputUser>11
+isCorrect = isKurangNol or (isLebihLima and isKurangDelapan) or isLebihSebelas
+print(isCorrect)
