b20a7f6fb0e42da8da509d91faae2c35215131ba
denyfebr <denyfebri@gamil.com>
2025-04-17 15:02:08 +0700

    Numpy matriks

--- 60 Package Numpy/main.py
+++ 60 Package Numpy/main.py
@@ -8,5 +8,19 @@ print(list_a)
 print(f"list_a = {list_a}")
 print(vector_a)
 print(f"vector_a = {vector_a}")
-print(vector_a**2)
+print(f"a pangkat 2 = {vector_a**2}")
+print(f"a kali 5 = {vector_a*5}")
+print(list_a*2) # ngaco hasilnya
 
+matrix_b = np.array([(1,2),(3,4)])
+print(f"matrix b = \n{matrix_b}")
+print(f"matrix b^2 = \n{matrix_b**2}")
+
+zeros_c = np.zeros((2,2))
+print(f"zeros c = \n{zeros_c}")
+
+ones_d = np.ones((2,2))
+print(f"ones d = \n{ones_d}")
+
+jumlah = matrix_b + matrix_b**2 + ones_d
+print(jumlah)
