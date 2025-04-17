d1612ebc9de06454b3a24203bfbc09fecae4034a
Deny Febriyanto <denyfebri@gmail.com>
2025-04-17 13:17:58 +0700

    Coba Numpy

---
+++ 60 Package Numpy/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+list_a = [1,2,3,4]
+vector_a = np.array([1, 2, 3, 4])
+
+print(list_a)
+# print(list_a**2) akan error
+print(f"list_a = {list_a}")
+print(vector_a)
+print(f"vector_a = {vector_a}")
+print(vector_a**2)
+
