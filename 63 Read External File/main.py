800a878aeb0f6e5470ccda0d23657bf1a4d9efb2
denyfebr <denyfebri@gamil.com>
2025-04-24 09:37:56 +0700

    Read file

--- 63 Read External File/main.py
+++ 63 Read External File/main.py
@@ -1,17 +1,21 @@
 ## Tutorial membaca file external
 
 print(3*"="," Membaca file txt ",3*"=")
-file = open("D:/Latihan/Python/Kelas_terbuka/Python-Dasar/63 Read External File/data.txt",mode="r")
+file = open("./63 Read External File/data.txt",mode="r")
 
 print(f"status read : {file.readable()}")
 print(f"status write : {file.writable()}")
 
 # baca seluruh file
-# print(file.read())
+print(file.read())
 
 # baca baris per-baris
 # print(file.readline()) # baca baris pertama
 # print(file.readline()) # baca baris kedua
 
 ## baca semua baris sebagai list
-print(file.readlines())
+# print(file.readlines())
+
+print(f"Apakah file sudah diclose : {file.closed}")
+file.close()
+print(f"Apakah file sudah diclose : {file.closed}")
